b5b8248d96a739eb772ad0c6508d221c68a50500
28sturchiox <28sturchiox@bwschools.net>
2025-03-04 09:30:10 -0500

    commit

---
+++ main.py
@@ -0,0 +1,45 @@
+import turtle
+
+screen = turtle.Screen()
+screen.screensize(500,500)
+screen.bgcolor("yellow")
+
+t = turtle.Turtle()
+
+t.penup()
+t.goto(0,0)
+t.pendown()
+t.pencolor("cyan")
+t.fillcolor("cyan")
+t.begin_fill()
+t.setheading(135)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.end_fill()
+t.penup()
+t.goto(0,0)
+t.pendown()
+t.pencolor("brown")
+t.fillcolor("brown")
+t.begin_fill()
+t.setheading(45)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.end_fill()
+
+turtle.done()
+
+
+
+
+
